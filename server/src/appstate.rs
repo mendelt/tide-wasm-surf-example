8bd27bbab8948ab54eb2116fbf9f53186a8b1f40
Mendelt Siebenga <msiebenga@gmail.com>
2020-10-01 23:06:00 +0200

    use sqlite database

--- server/src/appstate.rs
+++ server/src/appstate.rs
@@ -1,28 +1,28 @@
 use crate::{Config, Result};
-use sqlx::PgPool;
+use sqlx::SqlitePool;
 use tide::Request;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    database: PgPool,
+    database: SqlitePool,
     config: Config,
 }
 
 impl AppState {
     pub async fn init(config: Config) -> Result<Self> {
         Ok(AppState {
-            database: PgPool::connect(&config.database_url).await?,
+            database: SqlitePool::connect(&config.database_url).await?,
             config,
         })
     }
 }
 
 trait AppStateExt {
-    fn database(&self) -> PgPool;
+    fn database(&self) -> SqlitePool;
 }
 
 impl AppStateExt for Request<AppState> {
-    fn database(&self) -> PgPool {
+    fn database(&self) -> SqlitePool {
         todo!();
     }
 }
