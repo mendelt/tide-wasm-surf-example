a4e27ef59dd3b87863d1a20a9eec207fe6e21bd0
Mendelt Siebenga <msiebenga@gmail.com>
2020-10-17 14:07:52 +0200

    try to make a http request from the client

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,10 +1,15 @@
 use wasm_bindgen::prelude::*;
+use async_std::task;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
+
+    task::block_on(async {
+        surf::get("https://httpbin.org/get").await.unwrap();
+    });
 
     // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
